c32722f33f500c0f455817e734bd2d5200246a77
Kriwad <khatiwadakrish84@gmail.com>
2025-12-24 00:38:44 +0400

    some parser

---
+++ parser.py
@@ -0,0 +1,62 @@
+import pprint
+
+
+def parse_int(data, i):
+    assert data[i]== ord("i") 
+    i+=1
+    j = data.index(b"e" , i)
+    val = int(data[i : j])
+    return val , j+1
+
+def parse_str(data , i ):
+    j = data.index(b":" , i)
+    length = int(data[i: j])
+    j +=1 
+    val = data[j : j+length ]
+    return val  , j + length
+
+
+def parse_list(data , i):
+    assert data[i] == ord('l')
+    i+=1
+    arr= []
+    while data[i] != ord("e"):
+        val , i  = parse_any(data , i)
+        arr.append(val)
+    return arr , i+1
+
+def parse_dict(data , i ):
+    assert data[i]== ord('d')
+    i+=1 
+    d={}
+    while data[i] != ord('e'):
+        keys , i = parse_str(data , i )
+        value , i = parse_any(data , i)
+        d[keys] = value
+    return d , i+1
+
+def parse_any(data , i):
+    if data[i] == ord('l'):
+        return parse_list(data , i)
+    elif data[i] == ord('i'):
+        return parse_int(data , i)
+    elif data[i] == ord('d'):
+        return parse_dict(data , i)
+    elif chr(data[i]).isdigit():
+        return parse_str(data , i)
+    else:
+        return data , i
+
+def bencoding(data):
+    if isinstance(data , int):
+        return b"i"+str({data}).enocde()+b"e"
+    elif isinstance(data , str):
+        return str(len(data).encode()) + b":" + 
+    elif isinstance(data, str):
+        pass
+    elif isinstance(data , dict):
+        pass
+
+
+
+
