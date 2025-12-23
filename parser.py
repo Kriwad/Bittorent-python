abd68eb41e87f164ac0acfd03dda900c5c8dd4cc
Kriwad <khatiwadakrish84@gmail.com>
2025-12-24 03:09:57 +0400

    strict checking for parse int

--- parser.py
+++ parser.py
@@ -2,28 +2,31 @@ import pprint
 
 
 def parse_int(data, i):
-    assert data[i]== ord("i") 
+    assert data[i]== ord('i') 
     i+=1
-    j = data.index(b"e" , i)
+    j = data.index(b'e' , i)
     val = int(data[i : j])
     return val , j+1
 
 def parse_str(data , i ):
-    j = data.index(b":" , i)
+    j = data.index(b':' , i)
     length = int(data[i: j])
     j +=1 
     val = data[j : j+length ]
     return val  , j + length
-
+k = b"4:spam"
+print(parse_str(k,0))
 
 def parse_list(data , i):
     assert data[i] == ord('l')
     i+=1
     arr= []
-    while data[i] != ord("e"):
+    while data[i] != ord('e'):
         val , i  = parse_any(data , i)
         arr.append(val)
     return arr , i+1
+
+
 
 def parse_dict(data , i ):
     assert data[i]== ord('d')
@@ -47,16 +50,17 @@ def parse_any(data , i):
     else:
         return data , i
 
+
 def bencoding(data):
     if isinstance(data , int):
-        return b"i"+str({data}).enocde()+b"e"
-    elif isinstance(data , str):
-        return str(len(data).encode()) + b":" + 
-    elif isinstance(data, str):
-        pass
+        return f'i{data}e'.encode()
+    elif isinstance(data , bytes):
+        return f'{len(data)}:'.encode() + data
+    elif isinstance(data, list):
+        encoded_data = [bencoding(item) for item in data]
+        return b'l' + b"".join(encoded_data) +b'e'
     elif isinstance(data , dict):
         pass
-
 
 
 
